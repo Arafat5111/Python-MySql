f20beeb63746ee3cee6156e6a50cfc23ad04b297
Arafat <mdarafathosainchowdhory@gmail.com>
2023-10-22 00:55:51 +0600

    MySQL using Python

--- 6_Select_Data.py
+++ 6_Select_Data.py
@@ -3,6 +3,7 @@ con = mysql.connector.connect(host="localhost", user="arafat", passwd="5111", da
 
 mycursor = con.cursor()
 mycursor.execute("select * from employeeinfo")
+# mycursor.execute("select max(Salary) from employeeinfo")
 showResult = mycursor.fetchall()
 
 for row in showResult:
